f3bf59324ce9760864e7a103ea202e9f821c88e1
Diba <dibaaamir80@gmail.com>
2022-08-20 10:30:25 +0430

    [ADD] ng-zorro

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,20 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
 import {HomeModule} from './pages/home/home.module';
+import {NZ_I18N} from 'ng-zorro-antd/i18n';
+import {en_US} from 'ng-zorro-antd/i18n';
+import {registerLocaleData} from '@angular/common';
+import en from '@angular/common/locales/en';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, HomeModule],
-    providers: [],
+    imports: [BrowserModule, AppRoutingModule, HomeModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+    providers: [{provide: NZ_I18N, useValue: en_US}],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
