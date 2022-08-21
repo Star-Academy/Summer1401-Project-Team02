bdd77e33660e90898e6efef6e74fcc169a41750e
Sajad <s.soltaniant@gmail.com>
2022-08-21 10:30:48 +0430

    add basic routing to controller (to resolve exceptions)

--- Server/Server/Server/Controllers/DataInventoryController.cs
+++ Server/Server/Server/Controllers/DataInventoryController.cs
@@ -3,11 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 namespace Server.Controllers;
 
 [ApiController]
+[Route("[controller]/[Action]")]
 public class DataInventoryController : Controller
 {
 
     [HttpPost]
-    public IActionResult UploadFile(string data)
+    public IActionResult UploadFile()
     {
         return default;
         //returns table_name
@@ -18,7 +19,7 @@ public class DataInventoryController : Controller
         return default;
         //returns table_name
     }
-
+    
     public IActionResult DownlaodFile(string tableName, string fileFormat)
     {
         // service:
