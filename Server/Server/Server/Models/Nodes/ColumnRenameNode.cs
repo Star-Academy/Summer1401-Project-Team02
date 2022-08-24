dd4ba52f0a7f876aa4fdf2e0154e73ccc7ceaf47
matinmoradi110 <matinmoradi110@gmail.com>
2022-08-24 10:30:03 +0430

    ColumnRenameNode fixed

--- Server/Server/Server/Models/Nodes/ColumnRenameNode.cs
+++ Server/Server/Server/Models/Nodes/ColumnRenameNode.cs
@@ -4,22 +4,21 @@ namespace Server.Models.Nodes;
 
 public class ColumnRenameNode : ProcessorNode
 {
-    private readonly string _tableName;
     private readonly Dictionary<string, string> _oldAndNewNames;
 
-    public ColumnRenameNode(string id, List<string> previousNodesIds, string tableName,
+    public ColumnRenameNode(string id, List<string> previousNodesIds,
         Dictionary<string, string> oldAndNewNames) : base(id, previousNodesIds)
     {
-        _tableName = tableName;
         _oldAndNewNames = oldAndNewNames;
     }
 
     public override string Execute(ExecutionType executionType, Dictionary<string, Node> nodes)
     {
         string renamingQuery = "";
+        string tableName = nodes.GetValueOrDefault(_previousNodesIds.First()).Execute(executionType, nodes);
         foreach (var oldAndNewName in nodes)
         {
-            renamingQuery = renamingQuery + $"sp_rename '{_tableName}.{oldAndNewName.Key}', '{oldAndNewName.Value}', 'COLUMN';" +
+            renamingQuery = renamingQuery + $"sp_rename '{tableName}.{oldAndNewName.Key}', '{oldAndNewName.Value}', 'COLUMN';" +
                             "GO";
         }
 
