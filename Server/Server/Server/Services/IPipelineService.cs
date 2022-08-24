6f17980f3887a7779e09cef5c1d8a3cad1a33519
hosseinmasoodi <hosseinmasoodiii@gmail.com>
2022-08-24 12:01:40 +0430

    complete heading and api of preview(returns result as datatable)

--- Server/Server/Server/Services/IPipelineService.cs
+++ Server/Server/Server/Services/IPipelineService.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Server.Models;
 
 namespace Server.Services;
@@ -6,4 +7,6 @@ public interface IPipelineService
 {
     //throws exception
     public void Execute(Pipeline pipeline);
+    public DataTable Heading(Pipeline pipeline, string id);
+    public Tuple<DataTable, DataTable> Preview(Pipeline pipeline, string id);
 }
