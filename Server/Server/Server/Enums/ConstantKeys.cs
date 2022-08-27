dfb3df270abe723fbed24ee728aaf3d776929f65
Sajad <s.soltaniant@gmail.com>
2022-08-27 14:38:11 +0430

    add forgotten files

---
+++ Server/Server/Server/Enums/ConstantKeys.cs
@@ -0,0 +1,9 @@
+namespace Server.Enums;
+
+public static class ConstantKeys
+{
+    public const string TableName = "tableName";
+    public const string PreviousNode = "previousNode";
+    public const string Columns = "columns";
+    public const string Success = "success";
+}
