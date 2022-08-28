79dc264c45cdaa34a3f0c87f50b0ed84221a6d11
matinmoradi110 <matinmoradi110@gmail.com>
2022-08-28 15:19:26 +0430

    changed table name

--- Server/Server/Server/Config.cs
+++ Server/Server/Server/Config.cs
@@ -5,7 +5,7 @@ public static class Config
     public const string Server = "localhost";
     public const string DataBase = "version1";
     public const string Id = "sa";
-    public const string dataInventoryTableName = "sourceAndDestTables";
+    public const string dataInventoryTableName = "AllDatasetsInventory";
     public const string Password = "sajad 123";
     
     public const int PreviewCapacity = 5;
