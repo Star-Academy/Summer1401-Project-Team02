64fab34bc27e0479332dfd17b3a30fa86de8c297
hosseinmasoodi <hosseinmasoodiii@gmail.com>
2022-08-28 17:10:07 +0430

    merge with server

--- Server/Server/Server/Models/Nodes/ProcessorNode.cs
+++ Server/Server/Server/Models/Nodes/ProcessorNode.cs
@@ -10,7 +10,6 @@ public abstract class ProcessorNode : Node
     }
     public override string GetPreviousQueryString(ExecutionType executionType, Dictionary<string, Node?> nodes)
     {
-        var data = new JsonObject(Data);
-        return nodes.GetValueOrDefault(data.GetPreviousNode()).Execute(executionType, nodes);
+        return nodes!.GetValueOrDefault(_previousNode)!.Execute(executionType, nodes!);
     }
 }
