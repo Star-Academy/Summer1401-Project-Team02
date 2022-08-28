0fd8022d212d78afc3ef6c4452e37f4646276240
SeyyedMohammadHossein Hosseini <s.m.m.m.h2013@gmail.com>
2022-08-28 16:47:38 +0430

    Math Node

---
+++ Server/Server/Server/Enums/MathFunction.cs
@@ -0,0 +1,16 @@
+namespace Server.Enums;
+
+public enum MathFunction
+{
+    Abs,
+    Floor,
+    Ceiling,
+    Cos,
+    Cot,
+    Degrees,
+    Exp,
+    Log10,
+    Log,
+    Power,
+    Radians,
+}
