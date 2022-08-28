eb138816da3f87f4f3eb69a1c99d7e343f5eb152
Sajad <s.soltaniant@gmail.com>
2022-08-28 14:29:24 +0430

    fix a hidden bug in selection query :/

--- Server/Server/Server/Models/Nodes/ColumnSelectorNode.cs
+++ Server/Server/Server/Models/Nodes/ColumnSelectorNode.cs
@@ -9,7 +9,7 @@ public class ColumnSelectorNode : ProcessorNode
     public override string Execute(ExecutionType executionType, Dictionary<string, Node?> nodes)
     {
         if (executionType == ExecutionType.Heading && HeaderQueryString != null) return HeaderQueryString;
-        string answer = String.Format(QueryStrings.Selector, string.Join(", ", _columns),
+        string answer = String.Format(QueryStrings.Selector, string.Join(", ", _columns.Select(x => $"[{x}]")),
             nodes.GetValueOrDefault(_previousNode)!.Execute(executionType, nodes!));
         if (executionType == ExecutionType.Heading) HeaderQueryString = answer;
         return answer;
