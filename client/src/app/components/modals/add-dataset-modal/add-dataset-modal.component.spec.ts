d56a8550cf57293356ef779a0c5369cb63453001
Diba <dibaaamir80@gmail.com>
2022-08-28 16:49:22 +0430

    [FIX] prettier problems

--- client/src/app/components/modals/add-dataset-modal/add-dataset-modal.component.spec.ts
+++ client/src/app/components/modals/add-dataset-modal/add-dataset-modal.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AddDatasetModalComponent } from './add-dataset-modal.component';
+import {AddDatasetModalComponent} from './add-dataset-modal.component';
 
 describe('AddDatasetModalComponent', () => {
-  let component: AddDatasetModalComponent;
-  let fixture: ComponentFixture<AddDatasetModalComponent>;
+    let component: AddDatasetModalComponent;
+    let fixture: ComponentFixture<AddDatasetModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddDatasetModalComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AddDatasetModalComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddDatasetModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AddDatasetModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
