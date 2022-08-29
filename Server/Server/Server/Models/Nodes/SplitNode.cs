133c7d14e6b368f7d50c5302d0ef4429251f2b76
Sajad <s.soltaniant@gmail.com>
2022-08-29 14:16:43 +0430

    update split node, not completed yet

--- Server/Server/Server/Models/Nodes/SplitNode.cs
+++ Server/Server/Server/Models/Nodes/SplitNode.cs
@@ -8,13 +8,20 @@ public class SplitNode : ProcessorNode
     public string _delimeter;
     public List<string> _newNames;
 
+    
     public override string Execute(ExecutionType executionType, Dictionary<string, Node?> nodes)
     {
         var heading = Headers.ToList();
-        var index = 0;
+        var index = heading.IndexOf(_columnName);
         heading.Remove(_columnName);
-        heading.InsertRange(index, GenerateSplitQuery());
-        return string.Format(QueryStrings.Split, string.Join(", ", heading),
+
+        if (executionType == ExecutionType.Heading)
+        {
+            heading.AddRange(_newNames);
+        }
+        else heading.InsertRange(index, GenerateSplitQuery());
+
+        return string.Format(QueryStrings.Split, string.Join(", ", heading.Select(x => $"[{x}]")), 
             nodes.GetValueOrDefault(_previousNode).Execute(executionType, nodes));
     }
 
