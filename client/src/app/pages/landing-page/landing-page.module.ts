665a05f57f7a4b3f4503c86f851c6161bb55bc43
Diba <dibaaamir80@gmail.com>
2022-08-31 15:10:28 +0430

    [EDIT] landing page

--- client/src/app/pages/landing-page/landing-page.module.ts
+++ client/src/app/pages/landing-page/landing-page.module.ts
@@ -4,9 +4,12 @@ import {LandingPageComponent} from './landing-page.component';
 import {NzButtonModule} from 'ng-zorro-antd/button';
 import {RouterModule} from '@angular/router';
 import {MenuComponent} from './components/menu/menu.component';
+import {TeamComponent} from './components/team/team.component';
+import {CardComponent} from './components/team/components/card/card.component';
+import {NzCardModule} from 'ng-zorro-antd/card';
 
 @NgModule({
-    declarations: [LandingPageComponent, MenuComponent],
-    imports: [CommonModule, NzButtonModule, RouterModule],
+    declarations: [LandingPageComponent, MenuComponent, TeamComponent, CardComponent],
+    imports: [CommonModule, NzButtonModule, RouterModule, NzCardModule],
 })
 export class LandingPageModule {}
