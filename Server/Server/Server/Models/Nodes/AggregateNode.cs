c9aa52a4ba680c20ae7a055e917d4a3bfd99583e
Sajad <s.soltaniant@gmail.com>
2022-08-31 14:48:11 +0430

    fix and test aggregate node

--- Server/Server/Server/Models/Nodes/AggregateNode.cs
+++ Server/Server/Server/Models/Nodes/AggregateNode.cs
@@ -1,28 +1,32 @@
+using System.Linq;
 using Server.Enums;
 
 namespace Server.Models.Nodes;
 
 public class AggregateNode : ProcessorNode
 {
-    public List<Tuple<AggregateFunction, string, string>> _funtions;
+    public List<Tuple<AggregateFunction, string, string>> _functions;
     public List<string> _groupingColumns;
 
-    private string GenerateSelectingString()
+    private List<string> GenerateSelectingString()
     {
         var result = new List<string>();
-        foreach (var function in _funtions)
+        foreach (var function in _functions)
         {
-            var renaming = (function.Item3 == null ? $"as {function.Item2}" : $"as {function.Item3}");
+            var renaming = (function.Item3 == null ? $"as [{function.Item2}_{function.Item1}]" : $"as [{function.Item3}]");
             result.Add($"{function.Item1}([{function.Item2}]) {renaming}");
         }
 
-        return string.Join(", ", result);
+        return result;
     }
 
     public override string Execute(ExecutionType executionType, Dictionary<string, Node?> nodes)
     {
-        var groupingList = string.Join(", ", _groupingColumns.Select(x => $"[{x}]"));
+        var groupingList = _groupingColumns.Select(x => $"[{x}]");
+        var selection = GenerateSelectingString();
+        selection.AddRange(groupingList);
         var previousExecution = nodes.GetValueOrDefault(_previousNode).Execute(executionType, nodes);
-        return string.Format(QueryStrings.Aggregate, GenerateSelectingString() + groupingList, previousExecution, groupingList);
+        return string.Format(QueryStrings.Aggregate, string.Join(", ", selection), previousExecution, string.Join(", ", groupingList));
     }
 }
+
